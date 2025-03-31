b0c3d1bfe353f9f796ec620c4909708b8c02fe50
yumateb <yumateb@gmail.com>
2025-03-31 23:55:37 +0700

    fix(main-page): there is no change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
 					height={38}
 					priority
 				/>
+				<h1>why there is no change</h1>
 				<h1 className="text-4xl"> Update Front end</h1>
 				<ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
 					<li className="mb-2 tracking-[-.01em]">
